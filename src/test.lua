4cc435dac4d4a5c4671a054cbad5eabb81d33d11
wux4an <wux4an@duck.com>
2025-08-09 12:23:44 -0500

    remove test.lua from src

--- src/test.lua
+++
@@ -1,6 +0,0 @@
-local time = require("time")
-print("Ahora:", time.now())
-print("Hora formateada:", time.format("%A, %d %b %H:%M:%S %z", time.now()))
-print("Dormir un segundo")
-time.sleep(1000) -- duerme 1 segundo
-print("Listo!")
